33e167dcb4aee0ab95723fcfc95c0802167d9ef5
Jahnavi <jahnavibollineni26@gmail.com>
2024-10-03 23:48:12 -0400

    Update app.py

--- hw4/part3/app.py
+++ hw4/part3/app.py
@@ -1,22 +1,21 @@
 from transformers import pipeline
 import gradio as gr
 
-# Whisper model using Transformers pipeline
-asr = pipeline("automatic-speech-recognition", model="openai/whisper-small")
-
-
-# Function to handle the transcription
-def transcribe(audio):
-    result = asr(audio)["text"]
-    return result
-    
-# Gradio interface
-interface = gr.Interface(
-    fn=transcribe,
-    inputs=gr.Audio(type="filepath"),
-    outputs="text",
-    live=True
+# whisper-small model for transcription
+asr_pipeline = pipeline(task="automatic-speech-recognition", model="openai/whisper-small")
+ 
+def predict(input_audio):
+    # ASR pipeline to transcribe the audio
+    transcription = asr_pipeline(input_audio)["text"]
+    return transcription
+ 
+# Gradio app setup with audio input
+gradio_app = gr.Interface(
+    predict,
+    inputs=gr.Audio(label="Upload an audio file", sources=["upload","microphone"], type="filepath"),
+    outputs=gr.Textbox(label="Transcription"),
+    title="Speech-to-Text with Whisper Small",
 )
-
+ 
 if __name__ == "__main__":
-    interface.launch(share=True)
+    gradio_app.launch(share=True)
